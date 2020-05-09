12d6179fb9cc98acc805f263c7749c24f838caf1
Lillian Zhu <lilzhu8@stanford.edu>
2020-05-08 22:04:40 -0700

    handling files, still need cleaning

--- read_headlines.py
+++ read_headlines.py
@@ -1,12 +1,17 @@
 import json
 
+
+# reads items in json file as a list of dicts, where the entries are "article_link", "headline", "is_sarcastic"
 artList = []
-print("Started Reading JSON file which contains multiple JSON document")
 with open('data/headlines/Sarcasm_Headlines_Dataset.json') as f:
     for jsonObj in f:
         art = json.loads(jsonObj)
         artList.append(art)
 
-print("Printing each JSON Decoded Object")
-for art in artList[0:10]:
-    print(art["headline"], art["is_sarcastic"])
+# extract tweets as dict mapping tweet to sarcasm = 1, not sarcasm = 0 
+tweetlist = {}
+with open('data/twitter/sarcasm-dataset.txt', 'r') as fp:
+	for twt in fp:
+  		tweetlist[twt[0:len(twt)-2].strip()] = twt[-2]
+print(tweetlist)
+#TODO: get rid of html and other random junk strings
